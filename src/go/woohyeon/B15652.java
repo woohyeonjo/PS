69b7aea70242a1c8825387d925317695169360ad
woohyeonjo <effy25@naver.com>
2019-03-08 06:50:56 +0900

    2019-03-08

---
+++ src/go/woohyeon/B15652.java
@@ -0,0 +1,32 @@
+package go.woohyeon;
+
+import java.util.Scanner;
+
+public class B15652 {
+	static int N, M;
+	static StringBuilder sb;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		permutation(0);
+	}
+
+	private static void permutation(int index) {
+		
+		if(index == M) {
+			System.out.println(sb.toString());
+			return;
+		}
+		
+		for(int i = 1 ; i <= N ; ++i) {
+			sb.append(i + " ");
+			permutation(index + 1);
+			sb.deleteCharAt(sb.length() - 1);
+			sb.deleteCharAt(sb.length() - 1);
+		}
+	}
+}
--- src/go/woohyeon/B2636.java
+++ src/go/woohyeon/B2636.java
@@ -1,26 +1,27 @@
 package go.woohyeon;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class B2636 {
-//  1. 공기를 bfs 하여서 라벨링을 한다
-//  2. 라벨링된 공기와 접촉한 부분의 치즈를 라벨링한다.
-//  3. 라벨링된 치즈를 제거한다.
-//	4. 1 - 3 반복
-//	5. 계속해서 시간과 남은 라벨링 된 치즈를 저장한다.
 
 	static Cell[][] map;
 	static boolean[][] visited;
-	static int N, M;
+	static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	static Queue<Cell> q;
+	static int N, M, hour, cheeze, ans;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		q = new LinkedList<Cell>();
 		
 		N = sc.nextInt();
 		M = sc.nextInt();
 		
 		map = new Cell[N][M];
-		visited = new boolean[N][M];
+		hour = 0;
+		cheeze = 1;
 		
 		for(int row = 0 ; row < N ; ++row) {
 			for(int col = 0 ; col < M ; ++col) {
@@ -28,8 +29,45 @@ public class B2636 {
 			}
 		}
 		
+		while(cheeze > 0) {
+			if(cheeze > 0) ans = cheeze;
+			q.offer(map[0][0]);
+			bfs();
+		}
+		System.out.println(hour);
+		System.out.println(ans);
 	}
-	
+
+	private static void bfs() {
+		visited = new boolean[N][M];
+		visited[0][0] = true;
+		int cnt = 0;
+		while (!q.isEmpty()) {
+			Cell cell = q.poll();
+
+			int nr, nc;
+			for (int i = 0; i < 4; ++i) {
+				nr = cell.row + dir[i][0];
+				nc = cell.col + dir[i][1];
+				if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
+					Cell nCell = map[nr][nc];
+					if (!visited[nr][nc]) {
+						if (nCell.type == 1) {
+							nCell.type = 0;
+							cnt++;
+							visited[nr][nc] = true;
+						} else if (nCell.type == 0) {
+							visited[nr][nc] = true;
+							q.offer(nCell);
+						}
+					}
+				}
+			}
+		}
+		if(cnt > 0) hour++;
+		cheeze = cnt;
+	}
+
 	static class Cell {
 		int row, col, type;
 
