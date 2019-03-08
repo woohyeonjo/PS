f631bd1112b17175c3aefced11b2f5001cab5a0c
woohyeonjo <effy25@naver.com>
2019-03-08 20:09:41 +0900

    2018-03-08

--- src/go/woohyeon/B15655.java
+++ src/go/woohyeon/B15655.java
@@ -3,7 +3,7 @@ package go.woohyeon;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class B15654 {
+public class B15655 {
 	static int[] input;
 	static boolean[] selected;
 	static int N, M;
@@ -23,20 +23,20 @@ public class B15654 {
 		}
 		Arrays.sort(input);
 		
-		permutation(0);
+		permutation(0, 0);
 	}
-	private static void permutation(int index) {
+	private static void permutation(int index, int start) {
 		if(index == M) {
 			System.out.println(sb.toString().trim());
 			return;
 		}
 		
-		for(int i = 0 ; i < N ; ++i) {
+		for(int i = start ; i < N ; ++i) {
 			if(!selected[i]) {
 				int digit = (input[i] + "").length();
 				selected[i] = true;
 				sb.append(input[i] + " ");
-				permutation(index + 1);
+				permutation(index + 1, i + 1);
 				sb.delete(sb.length() - (digit + 1), sb.length());
 				selected[i] = false;
 			}
