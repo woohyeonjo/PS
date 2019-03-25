493b22609b0803b8b5ea0af12a6946f856a6dd32
woohyeonjo <effy25@naver.com>
2019-03-26 00:39:06 +0900

    start

---
+++ src/Algorithm/BOJ/go/B2442.java
@@ -0,0 +1,31 @@
+package Algorithm.BOJ.go;
+
+import java.util.Scanner;
+
+public class B2442 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int n = 1 ; n <= N ; ++n) {
+			for(int i = N - n ; i > 0 ; --i) {
+				System.out.print(" ");
+			}
+			for(int j = 0 ; j < 2 * n - 1 ; ++j) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int n = N ; n > 0 ; --n) {
+			for(int i = 1 ; i <= N - n ; ++i) {
+				System.out.print(" ");
+			}
+			for(int j = 0 ; j < 2 * n - 1 ; ++j) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/Algorithm/BOJ/go/B2445.java
@@ -0,0 +1,38 @@
+package Algorithm.BOJ.go;
+
+import java.util.Scanner;
+
+public class B2445 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int n = 1; n <= N; ++n) {
+			for(int i = 0 ; i < n ; ++i) {
+				System.out.print("*");
+			}
+			for(int j = 0 ; j < N * 2 - (n * 2) ; ++j) {
+				System.out.print(" ");
+			}
+			for(int i = 0 ; i < n ; ++i) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int n = 1; n <= N; ++n) {
+			for(int i = 0 ; i < N - n ; ++i) {
+				System.out.print("*");
+			}
+			for(int j = 0 ; j < n * 2  ; ++j) {
+				System.out.print(" ");
+			}
+			for(int i = 0 ; i < N - n ; ++i) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}
