d4b4daa01630a266759d02e3fca39bfc097886fe
woohyeonjo <effy25@naver.com>
2019-04-01 00:50:12 +0900

    2019-04-01

--- src/Algorithm/BOJ/go/B14890.java
+++ src/Algorithm/BOJ/go/B14890.java
@@ -3,88 +3,115 @@ package Algorithm.BOJ.go;
 import java.util.Scanner;
 
 public class B14890 {
+	
 	static int[][] map;
-	static int N, L, ans;
 	static boolean[] visited;
+	static int N, L, ans;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		N = sc.nextInt();
 		L = sc.nextInt();
-		ans = 0;
 		
 		map = new int[N][N];
-		for(int i = 0 ; i < N ; ++i) {
-			for(int j = 0 ; j < N ; ++j) {
-				map[i][j] = sc.nextInt();
+		
+		for(int r = 0 ; r < N ; ++r	) {
+			for(int c = 0 ; c < N ; ++c) {
+				map[r][c] = sc.nextInt();
 			}
 		}
 		
-		for(int i = 0 ; i < N ; ++i) {
-			rowCheck(i);
-			colCheck(i);
+		for(int r = 0 ; r < N ; ++r	) {
+			check(r);
 		}
 		
 		System.out.println(ans);
+		
 	}
-	
-	private static void colCheck(int col) {
+
+	private static void check(int idx) {
+		if(check_row(idx)) ans++;
+		if(check_col(idx)) ans++;
+	}
+
+	private static boolean check_row(int idx) {
 		visited = new boolean[N];
-		for(int row = 0 ; row < N - 1 ; ++row) {
-			if(Math.abs(map[row][col] - map[row + 1][col]) > 1) return;
-			if(map[row][col] - map[row + 1][col] == 1) {
-				int height = map[row + 1][col];
-				for(int l = 0 ; l < L ; ++l	) {
-					if(row + 1 + l >= N) return;
-					if(map[row + 1 + l][col] != height) return;
-				}
-				for(int l = 0 ; l < L ; ++l	) {
-					if(visited[row + 1 + l]) return;
-					visited[row + 1 + l] = true;
-				}
-			} else if(map[row][col] - map[row + 1][col] == -1) {
-				int height = map[row][col];
-				for(int l = 0 ; l < L ; ++l	) {
-					if(row - l < 0) return;
-					if(map[row - l][col] != height) return;
-				}
-				for(int l = 0 ; l < L ; ++l	) {
-					if(visited[row - l]) return;
-					visited[row - l] = true;
+		int cnt = 1;
+		
+		for(int c = 1 ; c < N ; ++c) {
+			if(map[idx][c] == map[idx][c - 1]) {
+				cnt++;
+				continue;
+			}
+			if(Math.abs(map[idx][c] - map[idx][c - 1]) != 1) return false;
+			if(map[idx][c] - map[idx][c - 1] == 1) {
+				if(cnt < L) return false;
+				cnt = 1;
+				for(int i = c - L ; i < c ; ++i) {
+					visited[i] = true;
 				}
 			}
 		}
-		System.out.println("col : " + col + "is possible");
-		ans++;
-	}
-	
-	private static void rowCheck(int row) {
-		visited = new boolean[N];
-		for(int col = 0 ; col < N - 1 ; ++col) {
-			if(Math.abs(map[row][col] - map[row][col + 1]) > 1) return;
-			if(map[row][col] - map[row][col + 1] == 1) {
-				int height = map[row][col + 1];
-				for(int l = 0 ; l < L ; ++l	) {
-					if(col + 1 + l >= N) return;
-					if(map[row][col + 1 + l] != height) return;
-				}
-				for(int l = 0 ; l < L ; ++l	) {
-					if(visited[col + 1 + l]) return;
-					visited[col + 1 + l] = true;
-				}
-			} else if(map[row][col] - map[row][col + 1] == -1) {
-				int height = map[row][col];
-				for(int l = 0 ; l < L ; ++l	) {
-					if(col - l < 0) return;
-					if(map[row][col - l] != height) return;
-				}
-				for(int l = 0 ; l < L ; ++l	) {
-					if(visited[col - l]) return;
-					visited[col - l] = true;
+		
+		cnt = 1;
+		
+		for(int c = N - 2 ; c >= 0 ; --c) {
+			
+			if(map[idx][c] == map[idx][c + 1]) {
+				cnt++;
+				continue;
+			}
+			if(Math.abs(map[idx][c] - map[idx][c + 1]) != 1) return false;
+			if(map[idx][c] - map[idx][c + 1] == 1) {
+				if(cnt < L) return false;
+				cnt = 1;
+				for(int i = c + 1 ; i <= c + L ; ++i) {
+					if(visited[i]) return false;
+					visited[i] = true;
 				}
 			}
 		}
-		System.out.println("row : " + row + "is possible");
-		ans++;
+		return true;
+	}
+
+	private static boolean check_col(int idx) {
+		visited = new boolean[N];
+		int cnt = 1;
+		
+		for(int r = 1 ; r < N ; ++r) {
+			if(map[r][idx] == map[r - 1][idx]) {
+				cnt++;
+				continue;
+			}
+			if(Math.abs(map[r][idx] - map[r - 1][idx]) != 1) return false;
+			if(map[r][idx] - map[r - 1][idx] == 1) {
+				if(cnt < L) return false;
+				cnt = 1;
+				for(int i = r - L ; i < r ; ++i) {
+					visited[i] = true;
+				}
+			}
+		}
+		
+		cnt = 1;
+		
+		for(int r = N - 2 ; r >= 0 ; --r) {
+			
+			if(map[r][idx] == map[r + 1][idx]) {
+				cnt++;
+				continue;
+			}
+			if(Math.abs(map[r][idx] - map[r + 1][idx]) != 1) return false;
+			if(map[r][idx] - map[r + 1][idx] == 1) {
+				if(cnt < L) return false;
+				cnt = 1;
+				for(int i = r + 1 ; i <= r + L ; ++i) {
+					if(visited[i]) return false;
+					visited[i] = true;
+				}
+			}
+		}
+		return true;
 	}
 }
