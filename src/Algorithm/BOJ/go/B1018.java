22f06f2a497e76b7f1de9b034a2b3e551552ca14
woohyeonjo <effy25@naver.com>
2019-04-04 01:12:27 +0900

    2019-04-04

---
+++ src/Algorithm/BOJ/go/B1018.java
@@ -0,0 +1,5 @@
+package Algorithm.BOJ.go;
+
+public class B1018 {
+
+}
--- src/Algorithm/SWEA/go/S5644.java
+++ src/Algorithm/SWEA/go/S5644.java
@@ -7,21 +7,22 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class S5644 {
-	
+
 	static Queue<Cell> q;
-	static int[][] dir = {{0, 0}, {-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	static int[][] dir = { { 0, 0 }, { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 	static Cell[][] map;
 	static int[] roadA;
 	static int[] roadB;
 	static int T, M, A, ans;
-	
-	static class BC implements Comparable<BC>{
+
+	static class BC implements Comparable<BC> {
 		int c, p;
 
 		public BC(int c, int p) {
 			this.c = c;
 			this.p = p;
 		}
+
 		@Override
 		public String toString() {
 			return this.p + "";
@@ -31,67 +32,67 @@ public class S5644 {
 		public int compareTo(BC o) {
 			return -(this.p - o.p);
 		}
-		
+
 	}
-	
-	static class Cell{
+
+	static class Cell {
 		int r, c;
 		ArrayList<BC> bcList;
-		
+
 		public Cell(int r, int c) {
 			this.r = r;
 			this.c = c;
 			bcList = new ArrayList<BC>();
 		}
-		
+
 		@Override
 		public String toString() {
 			return this.bcList.size() + "";
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		T = sc.nextInt(); 
-		for(int t = 1 ; t <= T ; ++t) {
+
+		T = sc.nextInt();
+		for (int t = 1; t <= T; ++t) {
 			init(sc);
 			go();
-			
+
 			System.out.println("#" + t + " " + ans);
 		}
 	}
-	
+
 	private static void go() {
 		int ap = 0, bp = 0;
-		if(map[1][1].bcList.size() > 0) {
-			ap = map[1][1].bcList.get(map[1][1].bcList.size() - 1).p;
+		if (map[1][1].bcList.size() > 0) {
+			ap = map[1][1].bcList.get(0).p;
 		}
-		if(map[10][10].bcList.size() > 0) {
-			bp = map[10][10].bcList.get(map[10][10].bcList.size() - 1).p;
+		if (map[10][10].bcList.size() > 0) {
+			bp = map[10][10].bcList.get(0).p;
 		}
 		int ar = 1, ac = 1;
 		int br = 10, bc = 10;
-		
-		for(int i = 0 ; i < M ; ++i) {
+
+		for (int i = 0; i < M; ++i) {
 			ar += dir[roadA[i]][0];
 			ac += dir[roadA[i]][1];
 			br += dir[roadB[i]][0];
 			bc += dir[roadB[i]][1];
-			
+
 			// A, B 둘다 충전 가능 지역일 때
-			if(map[ar][ac].bcList.size() > 0 && map[br][bc].bcList.size() > 0) {
+			if (map[ar][ac].bcList.size() > 0 && map[br][bc].bcList.size() > 0) {
 				// A, B 가 충전범위를 공유하지 않을 때
-				if(!isSameCoverage(map[ar][ac], map[br][bc])) {
+				if (!isSameCoverage(map[ar][ac], map[br][bc])) {
 					ap += map[ar][ac].bcList.get(0).p;
 					bp += map[br][bc].bcList.get(0).p;
 				} else {
-				// A, B가 충전범위를 공유할 때
-					if(map[ar][ac].bcList.size() == 1 && map[br][bc].bcList.size() == 1) {
+					// A, B가 충전범위를 공유할 때
+					if (map[ar][ac].bcList.size() == 1 && map[br][bc].bcList.size() == 1) {
 						ap += (map[ar][ac].bcList.get(0).p / 2);
 						bp += (map[br][bc].bcList.get(0).p / 2);
 					} else if (map[ar][ac].bcList.size() > 1 && map[br][bc].bcList.size() == 1) {
-						if(map[ar][ac].bcList.get(0).p == map[br][bc].bcList.get(0).p) {
+						if (map[ar][ac].bcList.get(0).p == map[br][bc].bcList.get(0).p) {
 							ap += map[ar][ac].bcList.get(1).p;
 							bp += map[br][bc].bcList.get(0).p;
 						} else {
@@ -99,7 +100,7 @@ public class S5644 {
 							bp += map[br][bc].bcList.get(0).p;
 						}
 					} else if (map[ar][ac].bcList.size() == 1 && map[br][bc].bcList.size() > 1) {
-						if(map[ar][ac].bcList.get(0).p == map[br][bc].bcList.get(0).p) {
+						if (map[ar][ac].bcList.get(0).p == map[br][bc].bcList.get(0).p) {
 							ap += map[ar][ac].bcList.get(0).p;
 							bp += map[br][bc].bcList.get(1).p;
 						} else {
@@ -107,32 +108,29 @@ public class S5644 {
 							bp += map[br][bc].bcList.get(0).p;
 						}
 					} else {
-						if(map[ar][ac].bcList.get(0).p == map[br][bc].bcList.get(0).p) {
-							ap += map[ar][ac].bcList.get(1).p;
-							bp += map[br][bc].bcList.get(0).p;
+						if (map[ar][ac].bcList.get(0).p == map[br][bc].bcList.get(0).p) {
+							if (map[ar][ac].bcList.get(0).p + map[br][bc].bcList.get(1).p > map[ar][ac].bcList.get(1).p
+									+ map[br][bc].bcList.get(0).p) {
+								ap += map[ar][ac].bcList.get(0).p;
+								bp += map[br][bc].bcList.get(1).p;
+							} else {
+								ap += map[ar][ac].bcList.get(1).p;
+								bp += map[br][bc].bcList.get(0).p;
+							}
 						} else {
 							ap += map[ar][ac].bcList.get(0).p;
 							bp += map[br][bc].bcList.get(0).p;
 						}
 					}
 				}
-			// A만 충전 가능 지역일 때
-			} else if(map[ar][ac].bcList.size() > 0) {
+				// A만 충전 가능 지역일 때
+			} else if (map[ar][ac].bcList.size() > 0) {
 				ap += map[ar][ac].bcList.get(0).p;
-				
-			// B만 충전 가능 지역일 때
-			} else if(map[br][bc].bcList.size() > 0) {
+
+				// B만 충전 가능 지역일 때
+			} else if (map[br][bc].bcList.size() > 0) {
 				bp += map[br][bc].bcList.get(0).p;
 			}
-			
-			System.out.println("==============================================");
-			System.out.println(ar + ", " + ac + "[" + roadA[i] + "]");
-			System.out.println(map[ar][ac].bcList.toString());
-			System.out.println(br + ", " + bc + "[" + roadB[i] + "]");
-			System.out.println(map[br][bc].bcList.toString());
-			System.out.println(ap + " " + bp + " = "  + (ap+bp));
-			System.out.println("==============================================");
-			System.out.println();
 		}
 		ans = ap + bp;
 	}
@@ -150,29 +148,29 @@ public class S5644 {
 	private static void init(Scanner sc) {
 		M = sc.nextInt(); // 총 이동 시간
 		A = sc.nextInt(); // BC의 개수
-		
+
 		roadA = new int[M];
 		roadB = new int[M];
 		map = new Cell[10 + 1][10 + 1];
 		q = new LinkedList<Cell>();
-		
+
 		// BC 맵 생성
-		for(int r = 1 ; r <= 10 ; ++r) {
-			for(int c = 1 ; c <= 10 ; ++c) {
+		for (int r = 1; r <= 10; ++r) {
+			for (int c = 1; c <= 10; ++c) {
 				map[r][c] = new Cell(r, c);
-			}				
+			}
 		}
-		
+
 		// 사용자 A, B 이동 정보 저장
-		for(int m = 0 ; m < M ; ++m) {
+		for (int m = 0; m < M; ++m) {
 			roadA[m] = sc.nextInt();
 		}
-		for(int m = 0 ; m < M ; ++m) {
+		for (int m = 0; m < M; ++m) {
 			roadB[m] = sc.nextInt();
 		}
-		
+
 		// BC 정보 저장
-		for(int a = 0 ; a < A ; ++a) {
+		for (int a = 0; a < A; ++a) {
 			int c = sc.nextInt();
 			int r = sc.nextInt();
 			int C = sc.nextInt();
@@ -181,10 +179,10 @@ public class S5644 {
 			q.offer(map[r][c]);
 			findCoverage(r, c, map[r][c].bcList.get(map[r][c].bcList.size() - 1));
 		}
-		
+
 		// 각 셀의 BC 리스트 P 내림차순으로 정렬
-		for(int r = 1 ; r <= 10 ; ++r) {
-			for(int c = 1 ; c <= 10 ; ++c) {
+		for (int r = 1; r <= 10; ++r) {
+			for (int c = 1; c <= 10; ++c) {
 				Collections.sort(map[r][c].bcList);
 			}
 		}
@@ -193,21 +191,23 @@ public class S5644 {
 	// BC 커버리지 범위 저장
 	private static void findCoverage(int r, int c, BC bc) {
 		int nr, nc;
-		
-		while(!q.isEmpty()) {
+
+		while (!q.isEmpty()) {
 			Cell cell = q.poll();
-			for(int i = 1 ; i < 5 ; ++i) {
+			for (int i = 1; i < 5; ++i) {
 				nr = cell.r + dir[i][0];
 				nc = cell.c + dir[i][1];
-				if(nr > 10 || nr < 1 || nc > 10 || nc < 1) continue;
-				if(Math.abs(r - nr) + Math.abs(c - nc) > bc.c) continue;
-				if(map[nr][nc].bcList.contains(bc)) continue;
+				if (nr > 10 || nr < 1 || nc > 10 || nc < 1)
+					continue;
+				if (Math.abs(r - nr) + Math.abs(c - nc) > bc.c)
+					continue;
+				if (map[nr][nc].bcList.contains(bc))
+					continue;
 				map[nr][nc].bcList.add(bc);
 				q.offer(map[nr][nc]);
 			}
 		}
-		
+
 	}
-	
-	
+
 }
