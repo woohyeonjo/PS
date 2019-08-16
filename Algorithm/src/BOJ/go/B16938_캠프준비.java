88f8b0c6dfe73ecf3a8ce0ee1eaa5f7a8dc88bb5
nnm <hyeon930@gmail.com>
2019-08-16 20:09:40 +0900

    스프링 부트 개발 중 공부

--- Algorithm/src/BOJ/go/B16938_캠프준비.java
+++ Algorithm/src/BOJ/go/B16938_캠프준비.java
@@ -4,16 +4,10 @@ import java.util.Scanner;
 
 public class B16938_캠프준비 {
 	
-	static class pair {
-		int x, y;
-		public pair(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+	static int N, L, R, X, ans;
+	static int[] level;
+	static boolean[] set;
 	
-	static int N, L, R, X, cnt;
-	static int[] numbers;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -21,19 +15,42 @@ public class B16938_캠프준비 {
 		L = sc.nextInt();
 		R = sc.nextInt();
 		X = sc.nextInt();
+
+		level = new int[N];
+		set = new boolean[N];
 		
-		numbers = new int[N];
+		for(int n = 0 ; n < N ; ++n) level[n] = sc.nextInt();
 		
-		for(int n = 0 ; n < N ; ++n) numbers[n] = sc.nextInt();
+		go(0);
+		System.out.println(ans);
+	}
+
+	private static void go(int cnt) {
+		if(cnt == N ) {
+			if(check()) ans++;
+			return;
+		}		
+		set[cnt] = true;
+		go(cnt + 1);
+		set[cnt] = false;
+		go(cnt + 1);
+		
+	}
+
+	private static boolean check() {
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
 		
 		for(int i = 0 ; i < N ; ++i) {
-			for(int j = i + 1 ; j < N ; ++j) {
-				if(numbers[i] + numbers[j] < L) continue;
-				if(numbers[i] + numbers[j] > R) continue;
-				if(Math.abs(numbers[i] - numbers[j]) < X) continue;
-				cnt++;
+			if(set[i]) {
+				sum += level[i];
+				min = min > level[i] ? level[i] : min;
+				max = max < level[i] ? level[i] : max;
 			}
 		}
-		System.out.println(cnt);
+		
+		if(sum >= L && sum <= R && (max - min) >= X)return true;
+		else return false;
 	}
 }
