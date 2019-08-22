1fe98da68e13e57731e908bac22c776db5875267
woohyeonjo <hyeon930@gmail.com>
2019-08-22 16:45:43 +0900

    algo

--- Algorithm/src/BOJ/go/B17142_연구소3.java
+++ Algorithm/src/BOJ/go/B17142_연구소3.java
@@ -21,14 +21,15 @@ public class B17142_연구소3 {
 		}
 	}
 	
-	static final String WALL = "-";
-	static final String NON_ACTIVE = "*";
-	static final String ACTIVE = "0";
+	static final int WALL = -1;
+	static final int NON_ACTIVE = -2;
+	static final int ACTIVE = -3;
 	
 	static int[][] lab;
-	static String[][] lab_copied;
-	static Cell[] selected;
+	static int[][] lab_copied;
+	static Cell[] selectedCell;
 	static boolean[][] visited;
+	static boolean[][] selected;
 	static PriorityQueue<Cell> q;
 	static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	static int N, M, ans, current;
@@ -41,9 +42,10 @@ public class B17142_연구소3 {
 		
 		lab = new int[N][N];
 		visited = new boolean[N][N];
+		selected = new boolean[N][N];
 		
 		q = new PriorityQueue<>();
-		selected = new Cell[M];
+		selectedCell = new Cell[M];
 		ans = Integer.MAX_VALUE;
 		current = Integer.MIN_VALUE;
 		
@@ -54,6 +56,7 @@ public class B17142_연구소3 {
 		}
 		
 		select(0, 0, 0);
+		if(ans == Integer.MAX_VALUE) ans = -1;
 		System.out.println(ans);
 		
 		
@@ -62,8 +65,13 @@ public class B17142_연구소3 {
 	private static void select(int sr, int sc, int cnt) {
 		if(cnt == M) {
 			copy();
-			for(int i = 0 ; i < M ; ++i) q.offer(new Cell(selected[i].r, selected[i].c, selected[i].time));
+			for(int i = 0 ; i < M ; ++i){
+				lab_copied[selectedCell[i].r][selectedCell[i].c] = ACTIVE;
+				visited[selectedCell[i].r][selectedCell[i].c] = true;
+				q.offer(new Cell(selectedCell[i].r, selectedCell[i].c, selectedCell[i].time));
+			}
 			bfs();
+			check();
 			return;
 		}
 		
@@ -76,16 +84,28 @@ public class B17142_연구소3 {
 					c = sc;
 					flag = false;
 				}
-				if(lab[r][c] == 2 && !visited[r][c]){
-					visited[r][c] = true;
-					selected[cnt] = new Cell(r, c, 0);
-					select(r, c, cnt + 1);
-					visited[r][c] = false;
+				if(lab[r][c] == 2 && !selected[r][c]){
+					selectedCell[cnt] = new Cell(r, c, 0);
+					select(r, c + 1, cnt + 1);
 				}
 			}
 		}
 	}
 	
+	private static void check() {
+		int max = 0;
+		
+		for(int r = 0; r < N ; ++r){
+			for(int c = 0 ; c < N ; ++c) {
+				if(lab_copied[r][c] < 0) continue;
+				if(lab_copied[r][c] == 0) return;
+				max = lab_copied[r][c] > max ? lab_copied[r][c] : max;
+			}
+		}
+		current = max;
+		ans = current < ans ? current : ans;
+	}
+
 	private static void bfs() {
 		while(!q.isEmpty()){
 			Cell cell = q.poll();
@@ -96,19 +116,31 @@ public class B17142_연구소3 {
 				nr = cell.r + dir[i][0];
 				nc = cell.c + dir[i][1];
 				
-				if(nr > N || nr < 0 || nc > N || nc < 0 || visited[nr][nc]) continue;
-				if(lab[nr][nc] == )) lab_copied
+				if(nr >= N || nr < 0 || nc >= N || nc < 0 || visited[nr][nc]) continue;
+				if(lab_copied[nr][nc] == WALL) continue;
+				if(lab_copied[nr][nc] == NON_ACTIVE){
+					visited[nr][nc] = true;
+					q.offer(new Cell(nr, nc, cell.time + 1));
+					continue;
+				}
+				
+				visited[nr][nc] = true;
+				lab_copied[nr][nc] = cell.time + 1;
+				q.offer(new Cell(nr, nc, cell.time + 1));
 			}
 		}
 		
 	}
 
 	private static void copy(){
+		lab_copied = new int[N][N];
 		visited = new boolean[N][N];
 		
 		for(int r = 0; r < N ; ++r){
 			for(int c = 0 ; c < N ; ++c) {
-				lab_copied[r][c] = lab[r][c] + "";
+				if(lab[r][c] == 1) lab_copied[r][c] = WALL;
+				else if(lab[r][c] == 2) lab_copied[r][c] = NON_ACTIVE;
+				else lab_copied[r][c] = 0;
 			}
 		}
 	}
---
+++ Algorithm/src/BOJ/go/B17142_연구소3_2.java
@@ -0,0 +1,131 @@
+package BOJ.go;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class B17142_연구소3_2 {
+	
+	static class Cell implements Comparable<Cell> {
+		int r, c, time;
+		
+		public Cell(int r, int c, int time) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.time = time;
+		}
+
+		@Override
+		public int compareTo(Cell o) {
+			return this.time - o.time;
+		}
+	}
+	
+	static ArrayList<Cell> virusSet;
+	static Cell[] selected;
+	static PriorityQueue<Cell> q;
+	static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	static int[][] map;
+	static int[][] copy;
+	static boolean[][] visited;
+	static int N, M, ans;
+	
+	static final int WALL = -3;
+	static final int NON_ACTIVE = -2;
+	static final int ACTIVE = -1;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		ans = Integer.MAX_VALUE;
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		selected = new Cell[M];
+		q = new PriorityQueue<>();
+		virusSet = new ArrayList<>();
+		
+		for(int r = 0 ; r < N ; ++r) {
+			for(int c = 0 ; c < N ; ++c) {
+				int temp = sc.nextInt();
+				if(temp == 2) virusSet.add(new Cell(r, c, 0));
+				map[r][c] = temp;
+			}
+		}
+		
+		select(0, 0);
+		if(ans == Integer.MAX_VALUE) ans = -1;
+		System.out.println(ans);
+	}
+
+	private static void select(int cnt, int index) {
+		
+		if(cnt == M){
+			copyMap();
+			
+			for(int i = 0 ; i < M ; ++i){
+				q.offer(new Cell(selected[i].r, selected[i].c, 0));
+				visited[selected[i].r][selected[i].c] = true;
+				copy[selected[i].r][selected[i].c] = ACTIVE;
+			}
+			
+			bfs();
+			check();
+			
+			return;
+		}
+		
+		for(int i = index ; i < virusSet.size() ; ++i){
+			selected[cnt] = virusSet.get(i);
+			select(cnt + 1, i + 1);
+		}
+	}
+
+	private static void check() {
+		int max = 0;
+		
+		for(int r = 0 ; r < N ; ++r) {
+			for(int c = 0 ; c < N ; ++c) {
+				if(copy[r][c] == 0) return;
+				if(copy[r][c] < 0) continue;
+				max = copy[r][c] > max ? copy[r][c] : max;
+			}
+		}
+		ans = ans > max ? max : ans;
+	}
+
+	private static void bfs() {
+		while(!q.isEmpty()){
+			Cell cell = q.poll();
+			
+			int nr, nc;
+			for(int i = 0 ; i < 4; ++i){
+				nr = cell.r + dir[i][0];
+				nc = cell.c + dir[i][1];
+				
+				if(nr >= N || nr < 0 || nc >= N || nc < 0 || visited[nr][nc]) continue;
+				if(copy[nr][nc] == WALL) continue;
+				if(copy[nr][nc] != NON_ACTIVE) copy[nr][nc] = cell.time + 1;
+				
+				q.offer(new Cell(nr, nc, cell.time + 1));
+				visited[nr][nc] = true;
+			}
+		}
+	}
+	
+	private static void copyMap() {
+		
+		copy = new int[N][N];
+		visited = new boolean[N][N];
+		
+		for(int r = 0 ; r < N ; ++r) {
+			for(int c = 0 ; c < N ; ++c) {
+				if(map[r][c] == 1) copy[r][c] = WALL;
+				else if(map[r][c] == 2) copy[r][c] = NON_ACTIVE;
+				else copy[r][c] = 0;
+			}
+		}
+	}
+}
