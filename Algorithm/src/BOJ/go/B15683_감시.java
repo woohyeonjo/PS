3847dada2de16277f6a5adfde51ff31cd63737d0
nnm <hyeon930@gmail.com>
2019-08-28 00:33:45 +0900

    algo

---
+++ Algorithm/src/BOJ/go/B15683_감시.java
@@ -0,0 +1,7 @@
+package BOJ.go;
+
+public class B15683_감시 {
+	public static void main(String[] args) {
+		
+	}
+}
--- Algorithm/src/BOJ/go/Main.java
+++ Algorithm/src/BOJ/go/Main.java
@@ -1,22 +1,46 @@
 package BOJ.go;
 
-import java.util.ArrayList;
-
 public class Main {
+	static int[][] map;
+	static int[] temp = new int[2];
     public static void main(String[] args) {
     	
-    	// 완전 동적
+    	map = new int[2][2];
+    	int num = 1;
     	
-    	ArrayList<ArrayList<Integer>> list;
-    	list = new ArrayList<>();
-    	
-    	// 반자동
-    	
-    	ArrayList<Integer>[] list2 = new ArrayList[10];
-    	
-    	for(int i = 0; i < 10; i++) {
-    		list2[i] = new ArrayList<>();
+    	for(int i = 0 ; i < 2 ; ++i) {
+    		for(int j = 0 ; j < 2 ; ++j) {
+    			map[i][j] = num++;
+    			System.out.print(map[i][j] + " ");
+    		}
+    		System.out.println();
     	}
+    	System.out.println();
     	
+    	go(0, 0, 0);
     }
+
+	private static void go(int depth, int I, int J) {
+		
+		if(depth == 2 || J == 2) {
+			for(int i = 0 ; i < 2 ; ++i) {
+				System.out.print(temp[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		boolean flag = true;
+		for(int i = 0 ; i < 2 ; ++i) {
+			for(int j = 0 ; j < 2 ; ++j) {
+				if(flag) {
+					i = I;
+					j = J;
+					flag=false;
+				}
+				temp[depth] = map[i][j];
+				go(depth + 1, i, j + 1);
+			}
+		}
+	}
 }
