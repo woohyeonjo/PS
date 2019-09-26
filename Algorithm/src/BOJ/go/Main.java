9bb38bc7cbde110b19cabf7f18d534b325814203
nnm <hyeon930@gmail.com>
2019-09-27 08:21:24 +0900

    algo

--- Algorithm/src/BOJ/go/Main.java
+++ Algorithm/src/BOJ/go/Main.java
@@ -4,29 +4,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        boolean[][] map = new boolean[100][100];
-        
-        int T = sc.nextInt();
-        
-        int R, C;
-        for(int t = 0 ; t < T ; ++t){
-        	C = sc.nextInt();
-        	R = sc.nextInt();
-        	for(int r = R ; r < R + 10 ; ++r){
-        		for(int c = C ; c < C + 10 ; ++c){
-        			map[r][c] = true;
-        		}
-        	}
-        }
-        
-        int ans = 0;
-        for(int r = 0 ; r < 100 ; ++r){
-            for(int c = 0 ; c < 100 ; ++c){
-                if(map[r][c])ans++;
-            }
-        }
-        System.out.println(ans);
+    	String s = "1234";
+    	
+    	for(int i = 0 ; i < s.length() ; ++i) {
+    		int c = (int) s.charAt(i);
+    		System.out.println(c);
+    	}
+    	
+    	
+    	int[] arr = {1, 2, 3, 4};
+    	
+    	Arrays.sort(arr);
     }
 }
