9657369e9ebab0d5674af0e491ce50a8ca2d747b
nnm <hyeon930@gmail.com>
2019-09-28 23:40:39 +0900

    algo

---
+++ Algorithm/src/CodingTest/Programmers/Solution3_190928.java
@@ -0,0 +1,15 @@
+package CodingTest.Programmers;
+
+public class Solution3_190928 {
+	public static void main(String[] args) {
+		int[] sticker = {12, 80, 14, 22, 100};
+		
+		int index = 1;
+		long[] tree = new long[(int) Math.pow(2, sticker.length) + 1];
+		
+	
+		
+		
+		
+	}
+}
